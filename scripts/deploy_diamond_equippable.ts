16861f778f240ae7c9be0bae17eae53be1ff2c88
Ayuilos <ekklovepeace@gmail.com>
2023-04-05 23:47:57 +0800

    feat: add `overrideOrAddOn` support to `oneTimeDeploy`

--- scripts/deploy_diamond_equippable.ts
+++ scripts/deploy_diamond_equippable.ts
@@ -40,21 +40,21 @@ export async function deployCreate2Deployer(usePreSign = true) {
   }
 
   if (usePreSign) {
-  // This pre-signed transaction is signed by 0xFBa50dD46Af71D60721C6E38F40Bce4d2416A34B,
-  // gasLimit at 300_000, gasPrice at 100Gwei.
-  // The EIP-155 is disabled to make sure transaction can be replayed on every single EVM chain,
-  // to let Create2Deployer address same and then every facet contract address will be same on
-  // every EVM chain.
-  // NOTE that now a lot of RPC nodes will not support pre-signed transaction without `chainId`,
-  // because it's a default config in go-ethereum client, to be able to send pre-signed transaction below,
-  // you can install `geth`(or similar client from other chain) yourself and add `--rpc.allow-unprotected-txs`
-  // flag when you run the full node, and you can also find and try available RPCs on https://chainlist.org.
+    // This pre-signed transaction is signed by 0xFBa50dD46Af71D60721C6E38F40Bce4d2416A34B,
+    // gasLimit at 300_000, gasPrice at 100Gwei.
+    // The EIP-155 is disabled to make sure transaction can be replayed on every single EVM chain,
+    // to let Create2Deployer address same and then every facet contract address will be same on
+    // every EVM chain.
+    // NOTE that now a lot of RPC nodes will not support pre-signed transaction without `chainId`,
+    // because it's a default config in go-ethereum client, to be able to send pre-signed transaction below,
+    // you can install `geth`(or similar client from other chain) yourself and add `--rpc.allow-unprotected-txs`
+    // flag when you run the full node, and you can also find and try available RPCs on https://chainlist.org.
 
-  const preSignedTransaction =
-    '0xf904158085174876e800830493e08080b903c2608060405234801561001057600080fd5b506103a2806100206000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c8063481286e61461004657806366cfa057146100755780637806530614610088575b600080fd5b61005961005436600461022b565b61009b565b6040516001600160a01b03909116815260200160405180910390f35b610059610083366004610263565b6100af565b610059610096366004610327565b6100c4565b60006100a88383306100c4565b9392505050565b60006100bc848484610121565b949350505050565b604080516001600160f81b03196020808301919091526bffffffffffffffffffffffff19606085901b16602183015260358201869052605580830186905283518084039091018152607590920190925280519101206000906100bc565b600080844710156101795760405162461bcd60e51b815260206004820152601d60248201527f437265617465323a20696e73756666696369656e742062616c616e636500000060448201526064015b60405180910390fd5b82516000036101ca5760405162461bcd60e51b815260206004820181905260248201527f437265617465323a2062797465636f6465206c656e677468206973207a65726f6044820152606401610170565b8383516020850187f590506001600160a01b0381166100bc5760405162461bcd60e51b815260206004820152601960248201527f437265617465323a204661696c6564206f6e206465706c6f79000000000000006044820152606401610170565b6000806040838503121561023e57600080fd5b50508035926020909101359150565b634e487b7160e01b600052604160045260246000fd5b60008060006060848603121561027857600080fd5b8335925060208401359150604084013567ffffffffffffffff8082111561029e57600080fd5b818601915086601f8301126102b257600080fd5b8135818111156102c4576102c461024d565b604051601f8201601f19908116603f011681019083821181831017156102ec576102ec61024d565b8160405282815289602084870101111561030557600080fd5b8260208601602083013760006020848301015280955050505050509250925092565b60008060006060848603121561033c57600080fd5b833592506020840135915060408401356001600160a01b038116811461036157600080fd5b80915050925092509256fea26469706673582212201f9fe2803ac8899d261e3d051dcdeb776303ddea50a3f731d492db8a2fab05f964736f6c634300081000331ba069cb3319d1af304d3e28e72959c77c4bdec0c32fe1dfe36809fa843406615411a02c7cb4752a8fdb2793e6b2659903aa95b8258123770a1ebdab66e901f56c883b';
-  const transactionRes = await ethers.provider.sendTransaction(preSignedTransaction);
+    const preSignedTransaction =
+      '0xf904158085174876e800830493e08080b903c2608060405234801561001057600080fd5b506103a2806100206000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c8063481286e61461004657806366cfa057146100755780637806530614610088575b600080fd5b61005961005436600461022b565b61009b565b6040516001600160a01b03909116815260200160405180910390f35b610059610083366004610263565b6100af565b610059610096366004610327565b6100c4565b60006100a88383306100c4565b9392505050565b60006100bc848484610121565b949350505050565b604080516001600160f81b03196020808301919091526bffffffffffffffffffffffff19606085901b16602183015260358201869052605580830186905283518084039091018152607590920190925280519101206000906100bc565b600080844710156101795760405162461bcd60e51b815260206004820152601d60248201527f437265617465323a20696e73756666696369656e742062616c616e636500000060448201526064015b60405180910390fd5b82516000036101ca5760405162461bcd60e51b815260206004820181905260248201527f437265617465323a2062797465636f6465206c656e677468206973207a65726f6044820152606401610170565b8383516020850187f590506001600160a01b0381166100bc5760405162461bcd60e51b815260206004820152601960248201527f437265617465323a204661696c6564206f6e206465706c6f79000000000000006044820152606401610170565b6000806040838503121561023e57600080fd5b50508035926020909101359150565b634e487b7160e01b600052604160045260246000fd5b60008060006060848603121561027857600080fd5b8335925060208401359150604084013567ffffffffffffffff8082111561029e57600080fd5b818601915086601f8301126102b257600080fd5b8135818111156102c4576102c461024d565b604051601f8201601f19908116603f011681019083821181831017156102ec576102ec61024d565b8160405282815289602084870101111561030557600080fd5b8260208601602083013760006020848301015280955050505050509250925092565b60008060006060848603121561033c57600080fd5b833592506020840135915060408401356001600160a01b038116811461036157600080fd5b80915050925092509256fea26469706673582212201f9fe2803ac8899d261e3d051dcdeb776303ddea50a3f731d492db8a2fab05f964736f6c634300081000331ba069cb3319d1af304d3e28e72959c77c4bdec0c32fe1dfe36809fa843406615411a02c7cb4752a8fdb2793e6b2659903aa95b8258123770a1ebdab66e901f56c883b';
+    const transactionRes = await ethers.provider.sendTransaction(preSignedTransaction);
 
-  console.log('Transaction Response is', transactionRes);
+    console.log('Transaction Response is', transactionRes);
   } else {
     console.log(
       'Will not use pre-signed transaction, make sure you use the correct create2Deployer address in the next deploying process',
@@ -77,11 +77,16 @@ export async function deployCreate2Deployer(usePreSign = true) {
  * @param create2DeployerAddress The deployer used for deploy contract in `CREATE2` way
  * @param deployed Mark if "the contract waiting to be deployed" is already `deployed`,
  * when set to `true`, this function will only compute the address of all facets.
+ * @param overrideOrAddOn Can be set to add-on / override some config, such as `toBeRemovedFunctions`, `FacetNames` etc.
  */
 export async function oneTimeDeploy(
   create2DeployerAddress: string,
   deployed = false,
   noNormalDeploy = false,
+  overrideOrAddOn?: {
+    addOn?: { toBeRemovedFunctions?: { [k: string]: string[] } };
+    override?: { FacetNames?: string[]; useNormalDeploy?: Record<string, boolean> };
+  },
 ) {
   const create2Deployer = await ethers.getContractAt('Create2Deployer', create2DeployerAddress);
   const RMRKMultiAssetRenderUtils = await ethers.getContractFactory('RMRKMultiAssetRenderUtils');
@@ -202,7 +207,7 @@ export async function oneTimeDeploy(
   console.log('');
   console.log('Deploying facets');
 
-  const FacetNames = [
+  let FacetNames = [
     'DiamondLoupeFacet',
     'LightmEquippableMultiAssetFacet',
     'LightmEquippableNestableFacet',
@@ -211,10 +216,16 @@ export async function oneTimeDeploy(
     'LightmMintModuleImplementer',
     'LightmImpl',
   ];
+  if (overrideOrAddOn?.override?.FacetNames) {
+    FacetNames = overrideOrAddOn.override.FacetNames;
+  }
 
-  const useNormalDeploy: { [k: string]: boolean } = {
+  let useNormalDeploy: { [k: string]: boolean } = {
     LightmImpl: true,
   };
+  if (overrideOrAddOn?.override?.useNormalDeploy) {
+    useNormalDeploy = overrideOrAddOn.override.useNormalDeploy;
+  }
 
   const constructorParams: { [k: string]: [any[], any[]] } = {
     LightmEquippableNestableFacet: [
@@ -263,6 +274,23 @@ export async function oneTimeDeploy(
       'isApprovedForAll(address,address)',
     ],
   };
+
+  if (overrideOrAddOn?.addOn?.toBeRemovedFunctions) {
+    for (const [facetName, functionSelectors] of Object.entries(
+      overrideOrAddOn.addOn.toBeRemovedFunctions,
+    )) {
+      if (facetName in toBeRemovedFunctions) {
+        const newToBeRemovedFunctionsOfFacet = new Set(toBeRemovedFunctions[facetName]);
+
+        for (const functionSelector of functionSelectors) {
+          newToBeRemovedFunctionsOfFacet.add(functionSelector);
+        }
+
+        toBeRemovedFunctions[facetName] = Array.from(newToBeRemovedFunctionsOfFacet);
+      }
+    }
+  }
+
   const cut = [];
   for (const FacetName of FacetNames) {
     const Facet = await (libraryLinking[FacetName]
